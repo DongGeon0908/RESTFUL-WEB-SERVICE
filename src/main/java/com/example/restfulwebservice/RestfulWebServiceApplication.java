692afeaca07fea6597607fd7e5fd431cd72b8ffd
DongGeon0908 <wrjssmjdhappy@gmail.com>
2021-10-14 18:17:26 +0900

    Exception Handler

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -2,6 +2,8 @@ package com.example.restfulwebservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServiceApplication {
@@ -9,5 +11,4 @@ public class RestfulWebServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
     }
-
 }
--- src/main/java/com/example/restfulwebservice/exception/CustomizeResponseEntityExceptionHandler.java
+++ src/main/java/com/example/restfulwebservice/exception/CustomizeResponseEntityExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.example.restfulwebservice.exception;
 
 import com.example.restfulwebservice.user.UserNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -14,7 +17,7 @@ import java.util.Date;
 // 모든 예외상황이 발생했을 떄 Handler를 통해 거쳐감~
 
 @RestController
-@ControllerAdvice
+@ControllerAdvice //--> @RestControllerAdvice
 public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(Exception.class)
@@ -31,4 +34,13 @@ public class CustomizeResponseEntityExceptionHandler extends ResponseEntityExcep
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
     }
 
+    // Valid Exception Handler!
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), "Validation Fail", ex.getBindingResult().toString());
+
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
 }
